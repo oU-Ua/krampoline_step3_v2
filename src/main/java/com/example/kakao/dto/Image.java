22c3fe624552f80c4bbdade69cb7b4dcaaf1c999
UU_jeong <80961726+oU-Ua@users.noreply.github.com>
2023-12-07 20:12:36 +0900

    commit

---
+++ src/main/java/com/example/kakao/dto/Image.java
@@ -0,0 +1,20 @@
+package com.example.kakao.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Image {
+    private String id;
+    private String image;
+    private int seed;
+    private Object nsfw_content_detected; // null을 처리하기 위해 Object를 사용
+    private Object nsfw_score; // null을 처리하기 위해 Object를 사용
+
+
+}
---
+++ src/main/java/com/example/kakao/dto/ImageRequestDTO.java
@@ -0,0 +1,14 @@
+package com.example.kakao.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ImageRequestDTO {
+    String prompt;
+}
